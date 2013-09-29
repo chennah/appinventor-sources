11178354afa9fc205771ddc3b31ec9f97f7dbbf7
chennah <chennah@mit.edu>
2013-09-29 09:37:32 -0400

    more comments

--- appinventor/blocklyeditor/src/language/common/lists.js
+++ appinventor/blocklyeditor/src/language/common/lists.js
@@ -183,7 +183,7 @@ Blockly.Language.lists_is_empty = {
 };
 
 Blockly.Language.lists_pick_an_item = {
-  // Length of list. a
+  // Length of list. Making a change here.
   category : Blockly.LANG_CATEGORY_LISTS,
   helpUrl : Blockly.LANG_LISTS_PICK_RANDOM_ITEM_HELPURL,
   init : function() {
